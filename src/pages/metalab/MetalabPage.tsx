2c10b821d29df2584b8dbd2b035ba28d2ed16f6a
Andrew Le Nguyen <anle.drewindeed@gmail.com>
2024-05-07 20:24:36 +0700

    feat: main titles

--- src/pages/metalab/MetalabPage.tsx
+++ src/pages/metalab/MetalabPage.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { useRef } from "react";
+import debounce from "lodash.debounce";
 
 import BgVideo from "@/assets/bg.mp4";
 import { Logo } from "@/assets/logo";
@@ -6,7 +8,6 @@ import { Mail } from "@/assets/mail";
 import MenuButton from "@/components/common/MenuButton";
 import useCurrentTime from "@/hooks/useCurrentTime";
 
-import { useRef } from "react";
 import "./index.css";
 
 export default function MetalabPage() {
@@ -20,25 +21,32 @@ export default function MetalabPage() {
   const menuItems = ["Uber", "Calvin Klein", "The Athletic"];
 
   // methods
-  const play = () => {
-    vidRef.current && vidRef.current.play();
-  };
-  const pause = () => {
-    vidRef.current && vidRef.current.pause();
-  };
+  const play = debounce(() => vidRef.current && vidRef.current.play(), 100);
+  const pause = debounce(() => vidRef.current && vidRef.current.pause(), 100);
 
   // render
   return (
     <div className="screen wrapper">
       {/* video background */}
       <div className="absolute screen">
-        <video ref={vidRef} src={BgVideo} autoPlay muted loop />
+        <video
+          className="brightness-75 contrast-125"
+          ref={vidRef}
+          src={BgVideo}
+          autoPlay
+          muted
+          loop
+        />
       </div>
 
+      {/* We make interface phrase */}
+      <h1 className="main-text flex-center bottom-[12rem] mr-[4rem] right-0 left-0">
+        We make
+      </h1>
+      <h1 className="main-text bottom-[6.5rem] left-[50%]">interfaces</h1>
+
       {/* navigation bar */}
-      <MenuButton className="fixed z-10 top-[1.5rem] left-[1.5rem]">
-        Menu
-      </MenuButton>
+      <MenuButton className="fixed top-[1.5rem] left-[1.5rem]">Menu</MenuButton>
       <div className="logo-wrapper">
         <Link to="/metalab">
           <Logo className="logo" />
@@ -52,7 +60,7 @@ export default function MetalabPage() {
               Get in Touch
             </div>
           }
-          className="p-1.5 ml-[2.5rem] w-[25px] h-[25px] hover:w-[105px] overflow-hidden"
+          className="p-1.5 ml-[0.75rem] w-[25px] h-[25px] hover:w-[105px] overflow-hidden"
           prefixClassName="shrink-0 fixed right-[16px] w-max pb-[1px]"
           childrenClassName="fixed right-[5.5px]"
         >
@@ -68,6 +76,8 @@ export default function MetalabPage() {
           </MenuButton>
         ))}
       </div>
+
+      {/* introduction */}
     </div>
   );
 }
